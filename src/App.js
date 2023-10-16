8fdca178fc9d3e54ef3cf54944d75e297f41f703
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2023-10-17 02:59:34 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import "./App.css";
 //import Button from "./containers/Select";
 //import Nav from "./containers/Nav";
 //import Overlay from "./containers/Overlay";
-import SearchView from "./views/searchView";
-import Crousal from "./containers/Crousal";
-import Nav from "./containers/Nav";
+//import SearchView from "./views/searchView";
+//import Crousal from "./containers/Crousal";
+// import Nav from "./containers/Nav";
+import Tag from "./components/Tag";
 //import Input from "./components/Input";
 function App() {
   return (
@@ -24,11 +25,11 @@ function App() {
         description={"hello my name is abuzar"}
         heading={"hindndndni"}
       ></Button> */}
-
-      <div>
+      <Tag value={"your tag goes here so yeah you couldnnnnnnnnnnnnnnnnnnnn"} />
+      {/* <div>
         <Nav Title={"Back"} />
         <Crousal />
-      </div>
+      </div> */}
     </div>
 
     //</div>
---
+++ src/components/Tag/tag.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./tag.styles.css";
+const Tag = ({ value }) => {
+  return (
+    <div className="tag-container">
+      <label>hello mu name is abuzar how are you are you good </label>
+    </div>
+  );
+};
+
+export default Tag;
